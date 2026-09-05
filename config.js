5543ad807372e8ac34e65792a7184aec00e67b18
Rishikeshsanin <141367936+Rishikeshsanin@users.noreply.github.com>
2026-09-06 01:15:26 +0530

    chore: add isolated vote service config

---
+++ config.js
@@ -0,0 +1,3 @@
+// Live global voting will be enabled by setting this to the isolated vote API URL.
+// Keep it blank for local preview mode. No secrets belong in this file.
+window.WYR_API_BASE = "";
